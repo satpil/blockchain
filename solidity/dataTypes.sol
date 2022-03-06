bd0793d292c1377c8ab2c564fc18c0ca9e73854f
satpil <82157294+satishrapid@users.noreply.github.com>
2022-03-06 18:07:48 +0530

    data types

--- solidity/dataTypes.sol
+++ solidity/dataTypes.sol
@@ -1,36 +1,41 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract myVariable {
+contract DataType {
+    uint256 public myInt;
 
-    // Integer
-    uint256 public myint;
-
-    function setInt(uint _myInt) public {
-        myint = _myInt;
+    // internal function cannot access outside
+    function internalCall(uint256 val) internal returns (uint256) {
+        return myInt = val;
     }
 
-    //Boolean
-    bool public myBool;
+    // external function cannot access inside the contract
 
-    function setBool(bool _myBool) public {
-        myBool = _myBool;
+    function externalCall(uint256 val) external returns (uint256) {
+        return myInt = val;
     }
 
-    //address
-    address public myAddress;
+    // private function cannot access outside or its inherit
 
-    function setAddress(address _myAddress) public {
-        myAddress = _myAddress;
+    function privateCall(uint256 val) private returns (uint256) {
+        return myInt = val;
     }
 
-    function getBalanceof() public view returns(uint){
-       return myAddress.balance;
+    // public function can access anywhere
+
+    function publicCall(uint256 val) public returns (uint256) {
+        return myInt = val;
     }
 
-    //string
-    string public myString;
+    // pure function cannot modified the state or red the state
 
-    function setString(string memory _mystring) public {
-        myString = _mystring;
+    function publicPureCall(uint256 val) public pure returns (uint256) {
+        return val * 2;
+    }
+
+    // pure function cannot modified the state but red the state
+
+    function publicViewCall(uint256 val) public view returns (uint256) {
+        return myInt + val;
     }
 }
